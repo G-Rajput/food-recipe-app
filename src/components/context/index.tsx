503b48817067ccc844b843cc8007c8bafa0ff784
Govind Rajput <govindrajput9819@gmail.com>
2024-04-14 18:36:53 +0530

    Api Integration

--- src/components/context/index.tsx
+++ src/components/context/index.tsx
@@ -8,8 +8,35 @@ export const GlobalContext = createContext<any>(null);
 
 const GlobalState: FC<GlobalStateProps> = ({ children }) => {
   const [searchParam, setSearchParam] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
+
+  async function handleSubmit(event: Event) {
+    event.preventDefault();
+    setLoading(true);
+    try {
+      const res = await fetch(
+        `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
+      );
+
+      const data = await res.json();
+      if (data?.data?.recipes) {
+        setRecipeList(data?.data?.recipes);
+        setLoading(false);
+        setSearchParam("");
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setSearchParam("");
+    }
+  }
+
+  console.log(loading, recipeList);
   return (
-    <GlobalContext.Provider value={{ searchParam, setSearchParam }}>
+    <GlobalContext.Provider
+      value={{ searchParam, loading, recipeList, setSearchParam, handleSubmit }}
+    >
       {children}
     </GlobalContext.Provider>
   );
